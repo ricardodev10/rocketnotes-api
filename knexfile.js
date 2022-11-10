650750a71f1cad7bd6e96d3b6e5695ff580b5f6c
Ricardo Junior <ricardodev10@yahoo.com>
2022-11-10 10:04:16 -0300

    :sparkles: feat: instaled and configured knex

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+const hash = require("hash")
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: hash.resolve(__dirname, "src", "database", "database.db")
+    },
+    useNullDefault: true
+  },
+};
