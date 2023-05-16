2c3b57598278c87948bda61f4100b867d61fd1e7
ricardo <ricardodev10@yahoo.com>
2023-05-16 12:35:13 -0300

    :sparkles: auth routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,15 @@
-const { Router } = require("express")
+const { Router } = require("express");
 
-const usersRouter = require("./users.routes")
-const notesRouter = require("./notes.routes")
-const tagsRouter = require("./tags.routes")
+const usersRouter = require("./users.routes");
+const notesRouter = require("./notes.routes");
+const tagsRouter = require("./tags.routes");
+const sessionsRouter = require("./sessions.routes");
 
-const routes = Router()
+const routes = Router();
 
-routes.use("/users", usersRouter)
-routes.use("/notes", notesRouter)
-routes.use("/tags", tagsRouter)
+routes.use("/users", usersRouter);
+routes.use("/notes", notesRouter);
+routes.use("/tags", tagsRouter);
+routes.use("/sessions", sessionsRouter);
 
-module.exports = routes
+module.exports = routes;
