66e06de8e224b81e0746c097660a1c2bb220b18c
ricardo <ricardodev10@yahoo.com>
2023-05-18 12:50:14 -0300

    :sparkles: using middleware

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,14 +1,17 @@
-const { Router } = require("express")
+const { Router } = require("express");
 
-const NotesController = require("../controllers/NotesController")
+const NotesController = require("../controllers/NotesController");
+const ensureAuthenticated = require("../middleware/ensureAuthenticated");
 
-const notesRoutes = Router()
+const notesRoutes = Router();
 
-const notesController = new NotesController()
+const notesController = new NotesController();
 
-notesRoutes.get("/", notesController.index)
-notesRoutes.post("/:user_id", notesController.create)
-notesRoutes.get("/:id", notesController.show)
-notesRoutes.delete("/:id", notesController.delete)
+notesRoutes.use(ensureAuthenticated);
 
-module.exports = notesRoutes
+notesRoutes.post("/", notesController.create);
+notesRoutes.get("/:id", notesController.show);
+notesRoutes.delete("/:id", notesController.delete);
+notesRoutes.get("/", notesController.index);
+
+module.exports = notesRoutes;
